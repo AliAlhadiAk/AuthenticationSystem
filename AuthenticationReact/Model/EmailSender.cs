e704e462bdba8e13cf3cfca552ab33fd57cf0856
AlialhadiAbokhalil <166250920+AliAlhadiAk@users.noreply.github.com>
2024-07-03 13:42:31 +0300

    Delete AuthenticationReact/Model/EmailSender.cs

--- AuthenticationReact/Model/EmailSender.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AuthenticationReact.Model
-{
-    public class EmailSender
-    {
-        public static string Email = "alialhadiabokhalil@gmail.com";
-        public static string Pass  = "Asp.net_c#123";
-    }
-}
